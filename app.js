8c261dc93a9c40be423f558af5f33c09ccfcc4e9
Fabian Jammal Ingram <68198938+fabianingram@users.noreply.github.com>
2020-09-13 15:55:13 -0700

    Add files via upload
    these files together  make a rock paper scissors game it it unfinished however.

---
+++ app.js
@@ -0,0 +1,75 @@
+const userScore = 0;
+const computerscore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_cpan = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById ("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice()  {
+        const choices = [ 'r', 'P', 'S'];
+        const randomNumber = Math.floor(Math.random() * 3);
+        return choices[randomNumber];
+    
+    }
+    function win(userChoice, computerChoice) {
+ userScore++;
+        userScore_span.innerHTML=userScore;
+        computerScore_span.innerHTML = computerScore;
+        console.log(user);
+        console.log(computer);
+
+   
+    }
+
+    function lose () {
+        console.log("LOST");
+    }
+
+    function draw() {
+        console.log("DRAWWW")
+    }
+
+function game(userChoice) {
+const computerChoice = getComputerChoice();
+switch (userChoice + computerChoice) {
+    case "rs":
+        case "pr":
+            case "sp":
+                win( userChoice + computerChoice );
+                break;
+                case "rp":
+                    case"ps":
+                    case "sr":
+                        lose(userChoice + computerChoice);
+                        break;
+                        case "rr":
+                            case"pp":
+                            case "ss":
+                               draw(userChoice + computerChoice);
+                                break;
+}
+
+}
+
+
+function main() {
+    rock_div.addEventListener('click', function () {
+        game("r");
+})
+
+
+paper_div.addEventListener('click', function() {
+   game("p");
+})
+
+scissors_div.addEventListener('click', function() {
+   game ("s");
+})
+
+}
+
+main();
+
